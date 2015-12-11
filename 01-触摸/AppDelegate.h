f48927c1483ac488ed4b3497fe19ec0deb5281e5
程玉娟 <1009449682@qq.com>
2015-12-11 15:25:39 +0800

    Initial Commit

---
+++ 01-触摸/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-触摸
+//
+//  Created by qingyun on 15/12/11.
+//  Copyright © 2015年 阿六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-触摸/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  01-触摸
+//
+//  Created by qingyun on 15/12/11.
+//  Copyright © 2015年 阿六. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
